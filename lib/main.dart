73641ddf5bb7aff4bf18b79d0d61bfe6b680c61c
incsmartfashion <incsmartfashion@gmail.com>
2021-08-21 04:42:13 -0700

    the challenge completed

---
+++ lib/main.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+import 'pages/productspage.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        //primarySwatch: Colors.white,
+      ),
+      home: MyHomePage(title: 'Buy Smart Phones'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white70,
+        centerTitle: true,
+        title: Text(
+          widget.title,
+          style: TextStyle(color: Colors.black),
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Get Latest Phones',
+            ),
+            ElevatedButton(
+              child:Text("view phones"),
+              onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => ProductsPage()),
+              );
+            })
+          ],
+        ),
+      ),
+    );
+  }
+}
